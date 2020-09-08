7e5a721b2979c4561fe98fb3602eb7309c4465ad
Gilia <Thomas.gilia@gmail.com>
2020-09-07 20:35:09 -0500

    commit after git crashed/removed index.lock. Code change was commenting the sass plugin that wasnt used

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,32 +5,33 @@
  */
 
 module.exports = {
-  siteMetadata: {
-    title: "Frozen Banana Treats",
-    description: "Simple recipe page with Gatsby-React, GraphQL, Contentful",
-    photoCredits: [
-      {
-        pic: "Bowl of banana ice cream",
-        source: "24COOKING-BANANA-ICE-CREAM1-articleLarge-v2.jpg"
-      },
-      {
-        pic: "banana pops",
-        source: "https://www.onelovelylife.com/healthy-chocolate-covered-bananas/"
-      },
-      {
-        pic: "PB banana balls",
-        source: "https://www.allrecipes.com/recipe/197331/high-protein-peanut-butter-balls/"
-      }
-    ]
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `5i4hvseuy8e2`,
-        accessToken: `Q4-fQH8lEEuJ0ijF3xinlfrJUr2ONRjHMIdbaBIBjyM`,
-      },
+    siteMetadata: {
+        title: "Frozen Banana Treats",
+        description: "Simple recipe page with Gatsby-React, GraphQL, Contentful",
+        photoCredits: [
+            {
+                pic: "Bowl of banana ice cream",
+                source: "24COOKING-BANANA-ICE-CREAM1-articleLarge-v2.jpg"
+            },
+            {
+                pic: "banana pops",
+                source: "https://www.onelovelylife.com/healthy-chocolate-covered-bananas/"
+            },
+            {
+                pic: "PB banana balls",
+                source: "https://www.allrecipes.com/recipe/197331/high-protein-peanut-butter-balls/"
+            }
+        ]
     },
-    `gatsby-plugin-sass`
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `5i4hvseuy8e2`,
+                accessToken: `Q4-fQH8lEEuJ0ijF3xinlfrJUr2ONRjHMIdbaBIBjyM`,
+            },
+        }
+    ],
 }
+
+  // `gatsby-plugin-sass`
